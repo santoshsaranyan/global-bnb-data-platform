ca725ff5a67e86c592745dc2d636890d1ee1ee1c
santoshsaranyan <saranyan.s@northeastern.edu>
2025-11-17 18:21:55 -0500

    modified silver reviews

--- dbt/global_bnb_dbt/models/silver/insideairbnb__stg_reviews.sql
+++ dbt/global_bnb_dbt/models/silver/insideairbnb__stg_reviews.sql
@@ -1,6 +1,21 @@
 WITH stg_reviews AS (
     SELECT
-        *
+        {{ dbt_utils.generate_surrogate_key([
+            'listing_id',
+            'id',
+            'date',
+            'city',
+            'country'
+        ]) }} AS review_id,
+        listing_id AS listing_cid,
+        id AS review_cid,
+        date::DATE AS review_date,
+        reviewer_id,
+        COALESCE(reviewer_name,'Unknown') as reviewer_name,
+        COALESCE(comments,'No comments provided') as comments,
+        city,
+        country,
+        extract_month
     FROM {{ source('bronze', 'insideairbnb__raw_reviews') }}
 )
 
